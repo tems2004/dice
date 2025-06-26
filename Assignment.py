9b25ca2cb6c0ae5c553182114ef71ad5afc7e2db
oluwapelumi temiloluwa <temiloluwaayomide52@gmail.com>
2025-06-26 22:59:40 +0100

    first commit

---
+++ Assignment.py
@@ -0,0 +1,78 @@
+# print ("QUESTION 1")
+# name = input("Enter your name? ")
+# age = int (input("Enter your age? "))
+# student = input("Are you a student? true/false: ")
+# Student = student.lower()
+# if age  == 25 and student == "True":
+#     print ("True")
+# else:
+#     print (f" {name} is {age} and a student : {Student}")
+
+# print ("QUESTION 2")
+# birthyear = int (input("Enter your birth year?: "))
+# age = 2025 - birthyear
+# message = "Your age is: " + str(age)
+# print (message)
+
+
+# print ("QUESTION 3")
+# name= input ("What is your name ? ")
+# number = int(input("What is your favourite number? "))
+# print(f"Hello, {name}! Your favourite number is {number}.")
+# print(f" Name length : {len(name)}") 
+
+# print ("QUESTION 4")
+# ticket = input("Do you have a ticket True/False?: ")
+# Ticket = ticket.lower()=="true"
+# age = int(input("How old are you?: "))
+# if Ticket == True and age > 16:
+#     print("Access Granted")
+# elif Ticket!= True and age < 16:
+#         print ("Access Denied")
+
+# print ("QUESTION 5")
+# grade = int(input("what is your score? :"))
+# if grade >= 90:
+#     Grade ="A"
+# elif grade > 80:
+#     Grade ="B"
+# elif grade > 70:
+#     Grade ="C"
+# elif grade > 60:
+#    Grade ="D"
+# else:
+#     grade < 60
+#     Grade ="F"
+# print(f"grades: {Grade}")
+
+# print ("QUESTION 6")
+# for num in range (1,16):
+#     if num == 10:
+#         break
+#     if num % 2==1:
+#         print(num)
+
+# print ("QUESTION 7")
+# grocery = ["milk", "bread", "eggs", "butter", "cheese"]
+# print("Second Item:", grocery[1])
+# print ("Last two:", grocery[3:5])
+# grocery[0]="Yogurt"
+# print ("Updated List:", (grocery))
+
+# print ("QUESTION 8")
+# list= ["Anna", "Ben", "Clara", "David"]
+# print("Number of guests:", len(list))
+# if "Clara" in list:
+#     print ("Clara: is Present")
+# else:
+#     print("Clara is not Present")
+# if "Eva" in list:
+#     print ("Eva is Present")
+# else:
+#     print("Eva: is not Present")
+# if "Eva" not in list:
+#     list.append("Eva")
+#     print("Updated list:", list)
+
+
+
